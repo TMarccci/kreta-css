2dab1f7966fc1ab133765df797756a8f93ff5997
Tihanyi Marcell <marcitihanyi@gmail.com>
2021-12-16 19:51:52 +0100

    Ud

--- site.js
+++ site.js
@@ -11,7 +11,7 @@
 // @grant      GM_addStyle
 // @updateURL  https://raw.githubusercontent.com/TMarccci/kreta-css/main/site.js
 // @downloadURL https://raw.githubusercontent.com/TMarccci/kreta-css/main/site.js
-// ==/UserScript==
+// ==/UserScript== 
 
 
 const my_css = GM_getResourceText("IMPORTED_CSS")
